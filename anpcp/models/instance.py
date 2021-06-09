5d61d432e2f16fc6e814b33c6f883bfd8d27f6a9
netotz <neto.otz@hotmail.com>
2021-06-08 21:17:24 -0500

    Add Instance model class

---
+++ anpcp/models/instance.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+from typing import List, Optional
+
+import numpy as np
+from scipy import spatial
+
+from .point import Point
+
+@dataclass()
+class Instance:
+    alpha: int
+    p: int
+    points: List[Point]
+    distances: Optional[np.ndarray] = None
+    
+    
+    def calculate_distances(self) -> None:
+        self.distances = spatial.distance_matrix(
+            ((p.x, p.y) for p in self.points),
+            ((p.x, p.y) for p in self.points),
+        )
