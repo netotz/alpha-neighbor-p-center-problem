11498a198ce6680885278e4f87bd5026a548801c
netotz <neto.otz@hotmail.com>
2021-06-15 12:53:21 -0500

    Define init method in Instance class

--- anpcp/models/instance.py
+++ anpcp/models/instance.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import List, Optional, Tuple
+from typing import List, Set, Tuple
 from random import randint
 
 import numpy as np
@@ -9,33 +9,43 @@ from .vertex import Vertex
 
 @dataclass
 class Instance:
-    p: int
-    alpha: int
-    vertexes: List[Vertex]
-    distances: Optional[np.ndarray] = None
+    def __init__(
+            self,
+            p: int,
+            alpha: int,
+            vertexes: List[Vertex],
+            with_distances: bool = True) -> None:
+        self.p = p
+        self.alpha = alpha
+        self.vertexes = vertexes
+        self.indexes = {v.index for v in self.vertexes}
+        if with_distances:
+            coords = [[v.x, v.y] for v in self.vertexes]
+            distances = spatial.distance_matrix(coords, coords)
+            self.distances = np.array([
+                [round(d) for d in row]
+                for row in distances
+            ])
+        else:
+            self.distances = None
 
 
     @classmethod
     def random(cls, n: int, p: int, alpha: int, x_max: int, y_max: int) -> 'Instance':
         coords = set()
         while len(coords) < n:
-            coords |= set(
+            coords |= {
                 (randint(0, x_max), randint(0, y_max))
                 for _ in range(n - len(coords))
-            )
+            }
         return Instance(
             p, alpha,
             [Vertex(i + 1, x, y) for i, (x, y) in enumerate(coords)]
         )
 
 
-    def calculate_distances(self) -> None:
-        coords = [[v.x, v.y] for v in self.vertexes]
-        distances = spatial.distance_matrix(coords, coords)
-        self.distances = np.array([
-            [round(d) for d in row]
-            for row in distances
-        ])
+    def get_indexes(self) -> Set[int]:
+        return {v.index for v in self.vertexes}
 
 
     def get_dist(self, fromindex: int, toindex: int) -> int:
