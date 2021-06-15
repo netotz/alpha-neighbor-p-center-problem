b3b1d10204757b27c2d2437397cdfdafd95df822
netotz <neto.otz@hotmail.com>
2021-06-15 13:01:23 -0500

    Add greedy heuristic function

--- anpcp/heuristics/constructive.py
+++ anpcp/heuristics/constructive.py
@@ -1,18 +1,32 @@
-from anpcp.models.instance import Instance
 from typing import Set
 
+from anpcp.utils import eval_obj_func
+from anpcp.models import Instance
 
-def pdp_based(instance: Instance) -> Set[int]:
-    vertexes = set(v.index for v in instance.vertexes)
+def pdp_based(instance: Instance, use_alpha_as_p: bool = False) -> Set[int]:
     solution = set(instance.get_farthest_indexes())
-    while len(solution) < instance.p:
-        index, _ = max((
+    p = instance.alpha if use_alpha_as_p else instance.p
+    while len(solution) < p:
+        index, dist = max((
                 (v, min(
                     instance.get_dist(v, s) for s in solution
                 ))
-                for v in vertexes - solution
+                for v in instance.indexes - solution
             ),
             key=lambda m: m[1]
         )
-        solution.add(index)
+        solution |= {index}
+    return solution
+
+
+def greedy(instance: Instance) -> Set[int]:
+    solution = pdp_based(instance, use_alpha_as_p=True)
+    while len(solution) < instance.p:
+        index, dist = min((
+                (v, eval_obj_func(instance, solution | {v}))
+                for v in instance.indexes - solution
+            ),
+            key=lambda m: m[1]
+        )
+        solution |= {index}
     return solution
