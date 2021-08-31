6f17ffe43f3041dc515788b68836c71c4fb09138
netotz <neto.otz@hotmail.com>
2021-08-31 09:17:28 -0500

    Add function for both first and best interchanges

--- anpcp/heuristics/local_search.py
+++ anpcp/heuristics/local_search.py
@@ -7,7 +7,7 @@ from models import Instance
 def interchange_first(instance: Instance, solution: Set[int]) -> Set[int]:
     max_alphath, obj_func = eval_obj_func(instance, solution)
 
-    is_improved = False
+    is_improved = True
     while is_improved:
         is_improved = False
         for index in instance.indexes - solution:
@@ -28,7 +28,7 @@ def interchange_first(instance: Instance, solution: Set[int]) -> Set[int]:
 def interchange_best(instance: Instance, solution: Set[int]) -> Set[int]:
     max_alphath, obj_func = eval_obj_func(instance, solution)
 
-    is_improved = False
+    is_improved = True
     while is_improved:
         current_best_solution = set(solution)
         current_best_alphath = max_alphath
@@ -43,6 +43,37 @@ def interchange_best(instance: Instance, solution: Set[int]) -> Set[int]:
                 current_best_alphath = new_alphath
                 current_best_obj_func = new_obj_func
 
+        is_improved = False
+        if current_best_obj_func < obj_func:
+            solution = set(current_best_solution)
+            max_alphath = current_best_alphath
+            obj_func = current_best_obj_func
+            is_improved = True
+
+    return solution
+
+
+def interchange_k(instance: Instance, solution: Set[int], is_first: bool) -> Set[int]:
+    max_alphath, obj_func = eval_obj_func(instance, solution)
+
+    is_improved = True
+    while is_improved:
+        current_best_solution = set(solution)
+        current_best_alphath = max_alphath
+        current_best_obj_func = obj_func
+
+        for index in instance.indexes - solution:
+            new_solution = solution - {max_alphath} | {index}
+            new_alphath, new_obj_func = eval_obj_func(instance, new_solution)
+
+            if new_obj_func < current_best_obj_func:
+                current_best_solution = set(new_solution)
+                current_best_alphath = new_alphath
+                current_best_obj_func = new_obj_func
+
+                if is_first:
+                    break
+        
         is_improved = False
         if current_best_obj_func < obj_func:
             solution = set(current_best_solution)
