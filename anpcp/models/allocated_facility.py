629eee22be307c46f847b8785ccdefce4b2ff41a
netotz <neto.otz@hotmail.com>
2022-02-09 15:54:54 -0600

    Improve docstring of `AllocatedFacility` class

--- anpcp/models/allocated_facility.py
+++ anpcp/models/allocated_facility.py
@@ -4,9 +4,9 @@ from dataclasses import dataclass
 @dataclass
 class AllocatedFacility:
     '''
-    Class that wraps a facility index and the distance to its allocated customer.
+    Data class designed to wrap a facility `index` and the `distance` to its allocated customer.
 
-    This object replaces the need to use a tuple[int, int].
+    This object only replaces the need to use a tuple[int, int].
     '''
     index: int
     distance: int
