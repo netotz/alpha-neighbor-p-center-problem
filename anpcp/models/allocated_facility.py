f6f63bfd84a808b3e4bd4db57925cbe729b08f57
netotz <neto.otz@hotmail.com>
2022-02-12 19:29:15 -0600

    Add customer field to AllocatedFacility

--- anpcp/models/allocated_facility.py
+++ anpcp/models/allocated_facility.py
@@ -4,9 +4,11 @@ from dataclasses import dataclass
 @dataclass
 class AllocatedFacility:
     '''
-    Data class designed to wrap a facility `index` and the `distance` to its allocated customer.
+    Data class designed to wrap a facility `index`,
+    its allocated `customer`, and their `distance`.
 
     This object only replaces the need to use a tuple[int, int].
     '''
     index: int
+    customer: int
     distance: int
