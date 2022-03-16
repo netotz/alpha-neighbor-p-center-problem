bf5a3849b3c23dde7db6c561a1d162085324ff86
netotz <neto.otz@hotmail.com>
2022-03-16 12:51:11 -0600

    chore: update debugger file

--- anpcp/debugger.py
+++ anpcp/debugger.py
@@ -1,10 +1,15 @@
 from models.instance import Instance
 from models.solver import Solver
 
-instance = Instance.random(100, 50)
-solver = Solver(instance, 5, 1, True)
+
+instance = Instance.random(1000, 500)
+solver = Solver(instance, 50, 3, True)
 solver.plot(with_annotations=False, dpi=250)
+solver.solution
+
 solver.fast_swap()
+solver.plot(with_annotations=False, dpi=250)
+
 # solver.grasp(1, 0.4)
 # args = dict(
 #     is_first=True,
