84a1a371b28507342d2852bf0ff40c4aa3f4e234
netotz <neto.otz@hotmail.com>
2022-03-29 10:32:52 -0600

    feat: move insert, remove and swap methods to Solution class

--- anpcp/models/solution.py
+++ anpcp/models/solution.py
@@ -21,3 +21,22 @@ class Solution:
         (not by calculating it).
         '''
         return self.critical_allocation.distance
+
+
+    def insert(self, facility: int) -> None:
+        self.closed_facilities.discard(facility)
+        self.open_facilities.add(facility)
+
+
+    def remove(self, facility: int) -> None:
+        self.open_facilities.discard(facility)
+        self.closed_facilities.add(facility)
+
+
+    def swap(self, facility_in: int, facility_out: int) -> None:
+        '''
+        Applies a swap to the solution by inserting `facility_in` to it
+        and removing `facility_out` from it.
+        '''
+        self.insert(facility_in)
+        self.remove(facility_out)
