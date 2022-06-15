38d03cd90372b750785436af5407f85e2c72fbf6
netotz <neto.otz@hotmail.com>
2022-06-15 17:14:47 -0500

    feat(grasp): complete method to compare betas

--- anpcp/utils.py
+++ anpcp/utils.py
@@ -38,8 +38,8 @@ def compare_local_search(solvers: Iterable[Solver], from_random: bool):
         datalist_ib.append(run_local_search(solver, False, initial))
 
     initial_header = "rand" if from_random else "RGD"
-    dataframe_if = create_dataframe(datalist_if, initial_header)
-    dataframe_ib = create_dataframe(datalist_ib, initial_header)
+    dataframe_if = create_dataframe_local_search(datalist_if, initial_header)
+    dataframe_ib = create_dataframe_local_search(datalist_ib, initial_header)
 
     return dataframe_if, dataframe_ib
 
@@ -80,13 +80,14 @@ def run_local_search(
     )
 
 
-def create_dataframe(data: Iterable[Tuple], initial_header: str):
+def create_dataframe_local_search(data: Iterable[Tuple], initial_header: str):
     dataframe = pd.DataFrame(
         data,
         columns="n m p alpha OF OF improvement time moves OF improvement time moves".split(),
     )
 
     mean_dataframe = dataframe.groupby("n m p alpha".split()).mean()
+    # create multiindex
     mean_dataframe = pd.concat(
         {
             initial_header: mean_dataframe.iloc[:, 0],
@@ -103,11 +104,33 @@ def format_latex_table(dataframe: pd.DataFrame, path: str):
     dataframe.to_latex(path, float_format="%.2f", multirow=True)
 
 
-def compare_betas(solvers: Iterable[Solver]):
+def compare_betas(solvers: Iterable[Solver], betas: Iterable[float]):
     """
     Compares the results of running GRASP with different values for beta (RGD).
     """
-    pass
+    datalist = list()
+
+    MAX_ITERS = 100
+
+    for solver in solvers:
+        for beta in betas:
+            solver.grasp(MAX_ITERS, beta)
+            obj_func = solver.solution.get_obj_func()
+
+            datalist.append(
+                (
+                    beta,
+                    solver.instance.n,
+                    solver.instance.m,
+                    solver.p,
+                    solver.alpha,
+                    obj_func,
+                    solver.solution.time,
+                )
+            )
+
+    dataframe = pd.DataFrame(datalist, columns="beta n m p alpha OF time".split())
+    return dataframe.groupby("beta n m p alpha".split()).mean()
 
 
 def get_stats_df(
