c969046f9a3a193020a827852b5a70f63d181ea7
netotz <neto.otz@hotmail.com>
2022-06-20 13:01:28 -0500

    feat(grasp): update debugger script

--- anpcp/debugger.py
+++ anpcp/debugger.py
@@ -1,15 +1,11 @@
 import os
-from utils import compare_local_search
 from models.instance import Instance
 from models.solver import Solver
 
-# filepath = os.path.abspath("data\\anpcp_50_50_0.json")
-# instance = Instance.read_json(filepath)
+filepath = os.path.abspath("..\\data\\ch150_100_50_0.anpcp.tsp")
+instance = Instance.read_tsp(filepath)
 
 instance = Instance.random(100, 100)
 solver = Solver(instance, 10, 2, True)
 
-# solver.construct()
-# solver.interchange(False)
-
-solver.fast_vertex_substitution(True)
+solver.grasp(50)
