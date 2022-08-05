ba5ea019a9214756c3ea32a204a26ce3bafb9ca7
netotz <neto.otz@hotmail.com>
2022-08-05 10:04:35 -0500

    feat(grasp): add file to calibrate GRASP iterations

---
+++ anpcp/grasp_iters.py
@@ -0,0 +1,33 @@
+import os
+
+import pandas as pd
+from anpcp.models.solver import Solver
+
+from models.instance import Instance
+
+
+def read_results():
+    filepath = ".\\nb_results\\grasp\\iters.pkl"
+    return pd.read_pickle(filepath)
+
+
+def __run():
+    datapath = os.path.abspath("..\\data")
+
+    name = "rl1323_882_441"
+    filepath = os.path.join(datapath, f"{name}_0.anpcp.tsp")
+    instance = Instance.read_tsp(filepath)
+    solver = Solver(instance, 44, 3)
+
+    MAX_ITERS = 5000
+    RAND_BETA = -1
+    results = solver.grasp_iters_detailed(MAX_ITERS, RAND_BETA)
+
+    filepath = ".\\nb_results\\grasp\\iters.pkl"
+    results.to_pickle(filepath)
+
+
+if __name__ == "__main__":
+    print("Running...")
+    __run()
+    print("Finished.")
