0b1d46a541dce14831ed9a9e13afb7be9c9ff54b
netotz <neto.otz@hotmail.com>
2022-08-17 20:38:32 -0500

    feat(grasp): add CLI to run iterations experiment

--- anpcp/grasp_iters.py
+++ anpcp/grasp_iters.py
@@ -1,6 +1,7 @@
 import os
 
 import pandas as pd
+import click
 
 from models.solver import Solver
 from models.instance import Instance
@@ -11,26 +12,28 @@ def read_results(instance_name: str):
     return pd.read_pickle(filepath)
 
 
-def __run():
+@click.command()
+@click.option("-n", "--name", required=True, help="instance name, without extension")
+@click.option("-p", "--p", type=int, required=True, help="p")
+@click.option("-a", "--alpha", type=int, required=True, help="alpha")
+@click.option(
+    "-i", "--iters", default=5000, show_default=True, help="maximum iterations"
+)
+@click.option("-b", "--beta", default=-1.0, show_default=True, help="beta for RCL")
+def __run(name: str, p: int, alpha: int, iters: int = 5000, beta: float = -1):
+    """
+    CLI to run iterations experiment on `name` instance.
+    """
     datapath = os.path.abspath("..\\data")
+    extension = ".json" if name.startswith("anpcp_") else ".anpcp.tsp"
+    filepath = os.path.join(datapath, f"{name}{extension}")
 
-    # TSPLIB instances
-    # name = "rl1323_882_441_0"
-    # filepath = os.path.join(datapath, f"{name}.anpcp.tsp")
-    # instance = Instance.read_tsp(filepath)
-
-    # random generated instances
-    name = "anpcp_882_441_0"
-    filepath = os.path.join(datapath, f"{name}.json")
     instance = Instance.read_json(filepath)
-
-    p = 20
-    alpha = 3
     solver = Solver(instance, p, alpha)
 
-    MAX_ITERS = 5000
-    RAND_BETA = -1
-    results = solver.grasp_iters_detailed(MAX_ITERS, RAND_BETA)
+    print("Running...")
+    results = solver.grasp_iters_detailed(iters, beta)
+    print("Finished.")
 
     filename = f"iters_{name}_p{p}_a{alpha}.pkl"
     filepath = f".\\nb_results\\grasp\\{filename}"
@@ -38,6 +41,4 @@ def __run():
 
 
 if __name__ == "__main__":
-    print("Running...")
     __run()
-    print("Finished.")
