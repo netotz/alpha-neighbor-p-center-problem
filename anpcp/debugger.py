c60d407f32da7094f92c6131aab64e335386fbfc
netotz <neto.otz@hotmail.com>
2022-10-29 23:06:52 -0500

    feat: modify debugger file

--- anpcp/debugger.py
+++ anpcp/debugger.py
@@ -3,20 +3,28 @@ import os
 from models.instance import Instance
 from models.solver import Solver
 
+from models.plotter import plot_solver
+
+#%%
 names = [
     "pr1002_668_334_4.anpcp.tsp",  # 0
     "rl1323_882_441_0.anpcp.tsp",
     "rat783_522_261_0.anpcp.tsp",  # 2
     "anpcp_882_441_0.json",
+    "dsj1000_750_250_0.anpcp.tsp",  # 4
+    "vm1748_1166_582_0.anpcp.tsp",
+    "rl1889_1260_629_0.anpcp.tsp",  # 6
+    "att48_32_16_0.anpcp.tsp",
+    "pr439_293_146_0.anpcp.tsp",  # 8
 ]
-filepath = os.path.abspath(f"..\\data\\{names[3]}")
-instance = (
-    Instance.read_tsp(filepath)
-    if filepath.endswith(".tsp")
-    else Instance.read_json(filepath)
-)
 
-solver = Solver(instance, 20, 3, False)
+#%%
+filepath = os.path.abspath(f"..\\data\\{names[8]}")
+instance = Instance.read(filepath)
+
+solver = Solver(instance, 21, 2, True)
 
 # solver.grasp_iters_detailed(10, -1)
 # solver.grasp(50)
+
+# %%
