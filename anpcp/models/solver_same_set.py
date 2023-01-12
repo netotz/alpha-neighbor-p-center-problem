a719684c57a6161b2c735f4b74dc3859db5a153d
netotz <neto.otz@hotmail.com>
2023-01-12 11:29:01 -0600

    feat(solver same set): override attribute's type

--- anpcp/models/solver_same_set.py
+++ anpcp/models/solver_same_set.py
@@ -1,9 +1,13 @@
 from dataclasses import dataclass
-from anpcp.models.solver import Solver
+
+from models.instance_same_set import InstanceSameSet
+from models.solver import Solver
 
 
 @dataclass
 class SolverSameSet(Solver):
+    instance: InstanceSameSet
+
     def get_users_indexes(self) -> set[int]:
         """
         Returns the indexes of closed facilities since U and F
