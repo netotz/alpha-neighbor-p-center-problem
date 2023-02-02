ec23c9dcb9c31ad4ab423ea6419f9a09bdcc48bd
netotz <neto.otz@hotmail.com>
2023-02-02 17:31:15 -0600

    feat(tabu_recency): add helper private methods

--- anpcp/models/tabu_structures/tabu_recency.py
+++ anpcp/models/tabu_structures/tabu_recency.py
@@ -5,8 +5,10 @@ from dataclasses import dataclass
 @dataclass
 class TabuRecency:
     """
-    Short term or recency based memory for intensification
-    in tabu search.
+    Short term or recency based memory for intensification in tabu search.
+
+    It's implemented with a queue of size `size` that stores tabu active facilities,
+    and with a set that stores the same facilities to check in O(1) if any of them is tabu.
     """
 
     size: int
@@ -16,12 +18,30 @@ class TabuRecency:
         self.__set = set[int]()
 
     def is_tabu(self, facility: int) -> bool:
+        """
+        Checks if `facility` is currently tabu active.
+        """
         return facility in self.__set
 
     def mark(self, facility: int) -> None:
-        if len(self.__queue) == self.size:
-            nontabu = self.__queue.popleft()
-            self.__set.remove(nontabu)
-
+        """
+        Marks `facility` as tabu active.
+        """
         self.__queue.append(facility)
         self.__set.add(facility)
+
+        self.__fix_queue()
+
+    def __fix_queue(self) -> None:
+        """
+        Private method to fix interal queue by dequeing until its size equals `size`.
+        """
+        while len(self.__queue) > self.size:
+            self.__dequeue()
+
+    def __dequeue(self) -> None:
+        """
+        Private method to dequeue the oldest facility and remove it from the set.
+        """
+        nontabu = self.__queue.popleft()
+        self.__set.remove(nontabu)
