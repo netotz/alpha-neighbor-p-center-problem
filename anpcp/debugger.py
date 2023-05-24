fccb44a4cb549c73575f9422a119c4a36feac9d2
netotz <neto.otz@hotmail.com>
2023-05-23 22:38:05 -0600

    feat(debugger): update debugger file

--- anpcp/debugger.py
+++ anpcp/debugger.py
@@ -7,7 +7,9 @@ from models.instance import Instance
 from models.solver import Solver
 from models.plotter import plot_solver
 
-DATA_PATH = os.path.join("..", "data")
+DATA_PATH = os.path.join(
+    "C:\\Users\\netoo\\local-personal\\repos\\alpha-neighbor-p-center-problem", "data"
+)
 
 # %%
 
@@ -24,22 +26,33 @@ names = [
     "rl1889_1260_629_0.anpcp.tsp",
     "att48_32_16_0.anpcp.tsp",  # 10
     "pr439_293_146_0.anpcp.tsp",
-    "ch150_100_50_0.anpcp.tsp",  # 12
+    "rat575_384_191_0.anpcp.tsp",  # 12
 ]
-filepath = os.path.join(DATA_PATH, names[11])
+filepath = os.path.join(DATA_PATH, names[6])
 instance = Instance.read(filepath)
-solver = Solver(instance, 21, 2)
+solver = Solver(instance, 44, 2, True)
 
-#%%
+# %%
+
+# solver.grasp(50, -1)
+# solver.construct()
+# solver.fast_vertex_substitution(True)
+# solver.tabu_search(2, 35)
+# print(solver.solution)
+
+# %%
 
 names_same = [
     "att48.tsp",  # 0
     "ch150.tsp",
     "pr439.tsp",  # 2
+    "rl1323.tsp",
 ]
 
 filepath = os.path.join(DATA_PATH, names_same[2])
 instance = InstanceSameSet.read(filepath)
-solver = SolverSameSet(instance, 10, 2)
+solver = SolverSameSet(instance, 40, 2)
+
+# solver.construct()
 
 # %%
