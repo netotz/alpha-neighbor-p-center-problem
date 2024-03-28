c21f5359367ede1fcd31be1f02a81bbab1065260
netotz <neto.otz@hotmail.com>
2024-03-27 21:58:58 -0600

    feat(cs): add Vertex model

---
+++ Anpcp/Anpcp.Core.UnitTests/Models/VertexTests.cs
@@ -0,0 +1,26 @@
+﻿using Anpcp.Core.Models;
+
+namespace Anpcp.Core.UnitTests.Models;
+
+public class VertexTests
+{
+    public static object[][] Data { get; } = [
+        // same x
+        [new Vertex(0, 0, 0), new Vertex(1, 0, 1), 1],
+        // same y
+        [new Vertex(0, 0, 0), new Vertex(1, 1, 0), 1],
+        // diagonals, from https://en.wikipedia.org/wiki/Pythagorean_triple#Examples
+        [new Vertex(0, 0, 0), new Vertex(1, 3, 4), 5],
+        [new Vertex(0, 0, 0), new Vertex(1, 5, 12), 13],
+        [new Vertex(0, 0, 0), new Vertex(1, 65, 72), 97],
+    ];
+
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void DistanceTo_ReturnsEuclideanDistance(Vertex v1, Vertex v2, double expectedDistance)
+    {
+        var actual = v1.DistanceTo(v2);
+
+        Assert.Equal(expectedDistance, actual);
+    }
+}
---
+++ Anpcp/Anpcp.Core/Models/Vertex.cs
@@ -0,0 +1,14 @@
+﻿namespace Anpcp.Core.Models;
+
+public record Vertex(int Index, int XCoord, int YCoord)
+{
+    /// <summary>
+    /// Calculates the Euclidean distance to another vertex.
+    /// </summary>
+    public double DistanceTo(Vertex v)
+    {
+        return Math.Sqrt(
+            Math.Pow(v.XCoord - XCoord, 2)
+            + Math.Pow(v.YCoord - YCoord, 2));
+    }
+}
