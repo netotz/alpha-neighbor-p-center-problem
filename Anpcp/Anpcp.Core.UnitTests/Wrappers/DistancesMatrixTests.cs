44e65fb52c64a4a0bad655a85f7b763a9b62008c
netotz <neto.otz@hotmail.com>
2024-03-27 22:46:36 -0600

    feat(cs): add `DistancesMatrix` wrapper

---
+++ Anpcp/Anpcp.Core.UnitTests/Wrappers/DistancesMatrixTests.cs
@@ -0,0 +1,45 @@
+﻿using Anpcp.Core.Models;
+using Anpcp.Core.Wrappers;
+
+namespace Anpcp.Core.UnitTests.Wrappers;
+
+public class DistancesMatrixTests
+{
+    public static object[][] Data { get; } = [
+        // 1 x 1, same set
+        [
+            new Vertex[] { new(0, 0, 0) },
+            new Vertex[] { new(0, 0, 0) },
+            new int[,] { { 0 } }
+        ],
+        // 2 x 2, different sets
+        [
+            new Vertex[] { new(0, 0, 0), new(1, 0, 1) },
+            new Vertex[] { new(0, 3, 4), new(1, 5, 12) },
+            new int[,] {
+                { 5, 13 },
+                { 4, 12 },
+            }
+        ],
+    ];
+
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void Constructor_InstantiatesDistancesMatrixCorrectly(Vertex[] v1, Vertex[] v2, int[,] expectedDistances)
+    {
+        var mockMatrix = new DistancesMatrix(v1, v2);
+
+        Assert.True(mockMatrix.IsInitialized);
+
+        var areEquals = new List<bool>();
+        for (var i = 0; i < v1.Length; i++)
+        {
+            for (var j = 0; j < v2.Length; j++)
+            {
+                areEquals.Add(mockMatrix[i, j] == expectedDistances[i, j]);
+            }
+        }
+
+        Assert.True(areEquals.All(a => a));
+    }
+}
---
+++ Anpcp/Anpcp.Core/Wrappers/DistancesMatrix.cs
@@ -0,0 +1,26 @@
+﻿using Anpcp.Core.Models;
+
+namespace Anpcp.Core.Wrappers;
+
+public class DistancesMatrix
+{
+    private readonly int[,] _distances;
+
+    public int this[int fromIndex, int toIndex]
+        => _distances[fromIndex, toIndex];
+
+    public bool IsInitialized => _distances.Length > 0;
+
+    public DistancesMatrix(Vertex[] vertices1, Vertex[] vertices2)
+    {
+        _distances = new int[vertices1.Length, vertices2.Length];
+
+        for (var i = 0; i < vertices1.Length; i++)
+        {
+            for (var j = 0; j < vertices2.Length; j++)
+            {
+                _distances[i, j] = (int)vertices1[i].DistanceTo(vertices2[j]);
+            }
+        }
+    }
+}
