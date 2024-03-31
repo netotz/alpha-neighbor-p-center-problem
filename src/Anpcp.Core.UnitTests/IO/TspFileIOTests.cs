1ba1e8405554b24148640ebd4f29119a21dd1f16
netotz <neto.otz@hotmail.com>
2024-03-30 21:49:37 -0600

    feat(cs): add `TspFileIO` class to read TSPLIB files

---
+++ src/Anpcp.Core.UnitTests/IO/TspFileIOTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.CompilerServices;
+
+using Anpcp.Core.Enums;
+using Anpcp.Core.IO;
+using Anpcp.Core.Models;
+
+namespace Anpcp.Core.UnitTests.IO;
+
+public class TspFileIOTests
+{
+    public static object[][] Data { get; } = [
+        [
+            GetAbsolutePath(@"Data\test1.anpcp.tsp"),
+            new List<Vertex> { new(1, 0, 0, VertexType.User) }
+        ],
+        [
+            GetAbsolutePath(@"Data\test2.anpcp.tsp"),
+            new List<Vertex> {
+                new(1, 1, 1, VertexType.User),
+                new(2, 2, 2, VertexType.User),
+                new(3, 3, 3, VertexType.Facility),
+                new(4, 4, 4, VertexType.Facility),
+            }
+        ],
+    ];
+
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void ReadNodes_ReturnsListOfVertex(string filePath, List<Vertex> expectedVertices)
+    {
+        var actualVertices = TspFileIO.ReadNodes(filePath);
+
+        Assert.Equal(actualVertices, expectedVertices);
+    }
+
+    private static string GetAbsolutePath(string filePath, [CallerFilePath] string currentPath = "")
+    {
+        var currentDirectory = Path.GetDirectoryName(currentPath) ?? "";
+
+        return Path.Combine(currentDirectory, filePath);
+    }
+}
---
+++ src/Anpcp.Core/IO/TspFileIO.cs
@@ -0,0 +1,43 @@
+﻿using Anpcp.Core.Enums;
+using Anpcp.Core.Models;
+
+namespace Anpcp.Core.IO;
+
+/// <summary>
+/// Reader for TSPLIB files.
+/// </summary>
+/// <remarks>
+/// Currently supports files that specify <c>NODE_COORDS_SECTION</c> only.
+/// </remarks>
+public static class TspFileIO
+{
+    private static string NodeCoordSection { get; } = "NODE_COORD_SECTION";
+    private static string Eof { get; } = "EOF";
+
+    public static List<Vertex> ReadNodes(string filePath)
+    {
+        using var streamReader = new StreamReader(filePath);
+
+        while (!streamReader.ReadLine()?.Contains(NodeCoordSection) ?? true) ;
+        // all the next lines should be node coords
+
+        var vertices = new List<Vertex>();
+
+        var line = streamReader.ReadLine() ?? "";
+        while (line != Eof)
+        {
+            line.Trim();
+
+            // each line has format "i x y t"
+            var items = line.Split();
+            var parsedItems = items.Select(int.Parse).ToArray();
+
+            vertices.Add(
+                new(parsedItems[0], parsedItems[1], parsedItems[2], (VertexType)parsedItems[3]));
+
+            line = streamReader.ReadLine() ?? "";
+        }
+
+        return vertices;
+    }
+}
