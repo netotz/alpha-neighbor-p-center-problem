581597690ab99e143f512ed7bee1f8ed6f9c2ea2
netotz <neto.otz@hotmail.com>
2024-04-02 12:32:25 -0600

    feat(cs): add `IHeuristic` contract

---
+++ src/Anpcp.Core/Heuristics/IHeuristic.cs
@@ -0,0 +1,18 @@
+﻿namespace Anpcp.Core.Heuristics;
+
+/// <summary>
+/// Heuristic algorithm contract.
+/// </summary>
+public interface IHeuristic
+{
+    /// <summary>
+    /// Problem parameter <c>p</c>, size of a solution <c>S</c>.
+    /// <c>p = |S|</c>
+    /// </summary>
+    int PSize { get; }
+    Instance Instance { get; }
+    /// <summary>
+    /// Seed for random generators. If <c>null</c>, a random one is used.
+    /// </summary>
+    int? Seed { get; }
+}
