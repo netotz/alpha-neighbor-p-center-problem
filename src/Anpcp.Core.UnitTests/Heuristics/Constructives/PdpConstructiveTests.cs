869a21aa376523f68c5355fb6d99cc1264e7a516
netotz <neto.otz@hotmail.com>
2024-04-02 12:33:26 -0600

    feat(cs): add `PdpConstructive` class

---
+++ src/Anpcp.Core.UnitTests/Heuristics/Constructives/PdpConstructiveTests.cs
@@ -0,0 +1,30 @@
+﻿using Anpcp.Core.Heuristics.Constructives;
+
+namespace Anpcp.Core.UnitTests.Heuristics.Constructives;
+
+public class PdpConstructiveTests
+{
+    public static int? TestSeed { get; } = 20240402;
+    public static Instance RandomInstance_m5 { get; } = new(0, 5, 100, 100, TestSeed);
+
+    public static object[][] Data { get; } = [
+        [RandomInstance_m5, 2, 2, 3],
+        [RandomInstance_m5, 4, 4, 1],
+    ];
+
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void Construct_ReturnsSolutionWithPFacilities(
+        Instance instance,
+        int p,
+        int expectedP,
+        int expectedCloseds)
+    {
+        var stubConstructive = new PdpConstructive(instance, p, TestSeed);
+
+        var mockSolution = stubConstructive.Construct();
+
+        Assert.Equal(mockSolution.OpenFacilities.Count, expectedP);
+        Assert.Equal(mockSolution.ClosedFacilities.Count, expectedCloseds);
+    }
+}
---
+++ src/Anpcp.Core/Heuristics/Constructives/PdpConstructive.cs
@@ -0,0 +1,69 @@
+﻿namespace Anpcp.Core.Heuristics.Constructives;
+
+/// <summary>
+/// Constructive heuristic to solve the p-dispersion problem (PDP).
+/// </summary>
+/// <remarks>
+/// This algorithm reduces the time complexity from O(mp**2) to O(mp).
+/// </remarks>
+public class PdpConstructive(Instance instance, int p, int? seed = null)
+    : IConstructive
+{
+    public int PSize { get; } = p;
+    public Instance Instance { get; } = instance;
+    public int? Seed { get; } = seed;
+
+    /// <summary>
+    /// Greedily constructs a solution for the PDP.
+    /// of the PDP.
+    /// </summary>
+    /// <remarks>Time O(mp)</remarks>
+    public Solution Construct()
+    {
+        // memory dictionary, O(m)
+        var distancesMemory = Enumerable
+            .Repeat(int.MaxValue, Instance.Facilities.Length)
+            .Select((v, i) => new
+            {
+                Key = i,
+                Value = v
+            })
+            .ToDictionary(
+                s => s.Key,
+                s => s.Value);
+
+        // O(m)
+        var solution = new Solution(Instance.FacilitiesIndices.ToHashSet());
+
+        var random = Seed is null
+            ? new Random()
+            : new Random(Seed.Value);
+
+        var lastInserted = random.GetItems(
+            solution.ClosedFacilities.ToArray(), 1)[0];
+
+        solution.Insert(lastInserted);
+        distancesMemory.Remove(lastInserted);
+
+        //// O(mp)
+        // O(p)
+        while (solution.OpenFacilities.Count < PSize)
+        {
+            // O(m)
+            foreach (var fi in solution.ClosedFacilities)
+                distancesMemory[fi] = Math.Min(
+                    distancesMemory[fi],
+                    Instance.DistancesFF[fi, lastInserted]);
+
+            // O(m)
+            lastInserted = distancesMemory
+                .MaxBy(p => p.Value)
+                .Key;
+
+            solution.Insert(lastInserted);
+            distancesMemory.Remove(lastInserted);
+        }
+
+        return solution;
+    }
+}
