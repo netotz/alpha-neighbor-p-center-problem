7590f51947915335dceaeadb453214cea0b19853
netotz <neto.otz@hotmail.com>
2024-04-02 12:32:35 -0600

    feat(cs): add `IConstructive` contract

---
+++ src/Anpcp.Core/Heuristics/Constructives/IConstructive.cs
@@ -0,0 +1,9 @@
+﻿namespace Anpcp.Core.Heuristics.Constructives;
+
+/// <summary>
+/// Constructive heuristic contract.
+/// </summary>
+public interface IConstructive : IHeuristic
+{
+    Solution Construct();
+}
