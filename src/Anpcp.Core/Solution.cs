bf3d65fb446c279342d8623910a438ebd70e2289
netotz <neto.otz@hotmail.com>
2024-04-02 12:32:12 -0600

    feat(cs): add `Solution` class

---
+++ src/Anpcp.Core/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace Anpcp.Core;
+
+public class Solution(HashSet<int> closedFacilities)
+{
+    public int ObjectiveFunctionValue { get; set; }
+    public HashSet<int> OpenFacilities { get; } = [];
+    public HashSet<int> ClosedFacilities { get; } = closedFacilities;
+
+    public void Insert(int facilityId)
+    {
+        ClosedFacilities.Remove(facilityId);
+        OpenFacilities.Add(facilityId);
+    }
+
+    public void Remove(int facilityId)
+    {
+        OpenFacilities.Remove(facilityId);
+        ClosedFacilities.Add(facilityId);
+    }
+
+    public void Swap(int facilityInId, int facilityOutId)
+    {
+        Insert(facilityInId);
+        Remove(facilityOutId);
+    }
+}
