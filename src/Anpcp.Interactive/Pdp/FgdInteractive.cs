ec88a57eac9637b99dc05def5bc2d385a8d66d2e
netotz <neto.otz@hotmail.com>
2024-10-15 22:57:08 -0500

    feat(interactive): add project for interactive code

---
+++ src/Anpcp.Interactive/Pdp/FgdInteractive.cs
@@ -0,0 +1,208 @@
+﻿using Anpcp.Core.Heuristics.Constructives;
+using Anpcp.Core.Instances;
+using Anpcp.Core.Solutions;
+
+using ScottPlot;
+
+using ColorName = System.Drawing.Color;
+
+namespace Anpcp.Interactive.Pdp;
+
+/// <summary>
+/// Distance from fi to the closest center from S.
+/// Type meant as the value of distances memory dictionary.
+/// </summary>
+public record ClosestCenter(int Distance, int Index = -1);
+
+/// <summary>
+/// Interactive version of <see cref="FgdConstructive"/> to generate visualizations.
+/// </summary>
+public class FgdInteractive
+    : IConstructive<InstanceSameSet, PdpSolution>
+{
+    public int PSize { get; }
+    public InstanceSameSet Instance { get; }
+    public int? Seed { get; }
+    private Random Random { get; }
+    public Dictionary<int, ClosestCenter> DistancesMemory { get; }
+    public PdpSolution Solution { get; }
+    public int LastInserted { get; set; }
+    /// <summary>
+    /// Current objective function value x(S).
+    /// </summary>
+    public int CurrentOfv { get; set; } = int.MaxValue;
+    private Queue<int> ClosedFacilitiesQueue { get; set; }
+    private int DequeuedFi { get; set; }
+
+    public FgdInteractive(int pSize, InstanceSameSet instance, int? seed)
+    {
+        PSize = pSize;
+        Instance = instance;
+        Seed = seed;
+        Random = Seed is null
+            ? new Random()
+            : new Random(Seed.Value);
+
+        // memory dictionary of minimum distances to S
+        // O(m)
+        DistancesMemory = Enumerable
+            .Repeat(int.MaxValue, Instance.Facilities.Length)
+            .Select((v, i) => new
+            {
+                Key = i,
+                Value = new ClosestCenter(v)
+            })
+            .ToDictionary(
+                s => s.Key,
+                s => s.Value);
+
+        Solution = new PdpSolution(Instance.FacilitiesIndices.ToHashSet());
+
+        Start();
+        HurryForMemory();
+    }
+
+    private void Start()
+    {
+        // start with random center
+        LastInserted = Random.GetItems(
+            Solution.ClosedFacilities.ToArray(), 1)[0];
+
+        Solution.Insert(LastInserted);
+        DistancesMemory.Remove(LastInserted);
+
+        ResetClosedFacilitiesQueue();
+    }
+
+    private void ResetClosedFacilitiesQueue()
+    {
+        ClosedFacilitiesQueue = new Queue<int>(Solution.ClosedFacilities);
+    }
+
+    /// <summary>
+    /// Tries to do an iteration of the main `while` loop of the algorithm.
+    /// </summary>
+    /// <returns>`false` if |S| = p</returns>
+    public bool TryIterateMain()
+    {
+        // S already has p centers
+        if (Solution.OpenFacilities.Count == PSize)
+        {
+            return false;
+        }
+
+        // if memory isn't fully updated
+        if (ClosedFacilitiesQueue.Count > 0)
+        {
+            return false;
+        }
+
+        // get farthest facility to S
+        // O(m - p) ~= O(m)
+        LastInserted = DistancesMemory
+            .MaxBy(p => p.Value)
+            .Key;
+
+        Solution.Insert(LastInserted);
+
+        // update x(S)
+        CurrentOfv = Math.Min(
+            CurrentOfv,
+            DistancesMemory[LastInserted].Distance);
+
+        DistancesMemory.Remove(LastInserted);
+
+        ResetClosedFacilitiesQueue();
+
+        return true;
+    }
+
+    /// <summary>
+    /// Tries to plot an iteration of the `for` loop that updates the memory.
+    /// </summary>
+    /// <returns>`null` if memory is completely updated.</returns>
+    public Plot? TryPlotForMemoryIteration(bool isHurrying = false)
+    {
+        // if empty
+        if (!ClosedFacilitiesQueue.TryDequeue(out var fi))
+        {
+            return null;
+        }
+
+        var closestCenter = DistancesMemory[fi];
+        var liDistance = Instance.DistancesFF[fi, LastInserted];
+        var minDistance = Math.Min(closestCenter.Distance, liDistance);
+
+        // only update if necessary (li < cc)
+        if (minDistance == liDistance)
+        {
+            DistancesMemory[fi] = new(minDistance, LastInserted);
+        }
+
+        if (isHurrying)
+        {
+            return null;
+        }
+
+        var plot = new Plot();
+
+        // closed facilities
+        var cfVertices = Instance.Facilities
+            .Where(f => Solution.ClosedFacilities.Contains(f.Index));
+        plot.Add.ScatterPoints(
+            cfVertices.Select(v => (double)v.XCoord).ToArray(),
+            cfVertices.Select(v => (double)v.YCoord).ToArray(),
+            new(ColorName.LightGray));
+
+        // centers
+        var sVertices = Instance.Facilities
+            .Where(f => Solution.OpenFacilities.Contains(f.Index)
+                && f.Index != LastInserted);
+        plot.Add.ScatterPoints(
+            sVertices.Select(v => (double)v.XCoord).ToArray(),
+            sVertices.Select(v => (double)v.YCoord).ToArray(),
+            new(ColorName.Red));
+
+        var fiVertex = Instance.Facilities[fi];
+        plot.Add.ScatterPoints(
+            (double[])[fiVertex.XCoord],
+            (double[])[fiVertex.YCoord],
+            new Color(ColorName.Yellow));
+
+        var liVertex = Instance.Facilities[LastInserted];
+        plot.Add.ScatterPoints(
+            (double[])[liVertex.XCoord],
+            (double[])[liVertex.YCoord],
+            new(ColorName.LightBlue));
+
+        var ccVertex = Instance.Facilities[closestCenter.Index];
+        plot.Add.ScatterPoints(
+            (double[])[ccVertex.XCoord],
+            (double[])[ccVertex.YCoord],
+            new(ColorName.LightGreen));
+
+        return plot;
+    }
+
+    /// <summary>
+    /// Runs all iterations left to fully update memory.
+    /// </summary>
+    public void HurryForMemory()
+    {
+        while (ClosedFacilitiesQueue.Count > 0)
+        {
+            TryPlotForMemoryIteration(true);
+        }
+
+        ResetClosedFacilitiesQueue();
+    }
+
+    /// <summary>
+    /// Do not call, throws exception.
+    /// It must be here because of the interface contract.
+    /// </summary>
+    public PdpSolution Construct()
+    {
+        throw new NotImplementedException();
+    }
+}
