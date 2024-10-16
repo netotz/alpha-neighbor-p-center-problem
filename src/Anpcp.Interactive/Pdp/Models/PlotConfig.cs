919d3f08fa97e8bd7c4414dee7327f0e0651affa
netotz <neto.otz@hotmail.com>
2024-10-16 01:55:48 -0500

    feat(interactive): add docstrings for `PlotConfig` props

--- src/Anpcp.Interactive/Pdp/Models/PlotConfig.cs
+++ src/Anpcp.Interactive/Pdp/Models/PlotConfig.cs
@@ -8,10 +8,25 @@ public record PlotConfig
     public double ScaleFactor { get; init; } = 1;
     public float MarkerSize { get; init; } = 1;
     public float MarkerOutlineWidth { get; init; } = 1;
+    /// <summary>
+    /// Color for closed facilities markers.
+    /// </summary>
     public ColorName CfColor { get; init; } = ColorName.Gray;
+    /// <summary>
+    /// Color for solution markers.
+    /// </summary>
     public ColorName SColor { get; init; } = ColorName.Red;
+    /// <summary>
+    /// Color for facility to insert (fi) marker.
+    /// </summary>
     public ColorName FiColor { get; init; } = ColorName.Yellow;
+    /// <summary>
+    /// Color for last inserted (li) facility marker.
+    /// </summary>
     public ColorName LiColor { get; init; } = ColorName.Blue;
+    /// <summary>
+    /// Color for closest center (cc) marker to fi.
+    /// </summary>
     public ColorName CcColor { get; init; } = ColorName.Green;
 }
 
