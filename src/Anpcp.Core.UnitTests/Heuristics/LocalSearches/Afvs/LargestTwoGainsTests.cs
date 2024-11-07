93e3578281afa294eb007acd3cd7f70ecee9d76e
netotz <neto.otz@hotmail.com>
2024-11-07 13:34:16 -0600

    feat(afvs): add class for largest two gains

---
+++ src/Anpcp.Core.UnitTests/Heuristics/LocalSearches/Afvs/LargestTwoGainsTests.cs
@@ -0,0 +1,46 @@
+﻿using Anpcp.Core.Heuristics.LocalSearches.Afvs.Models;
+using Anpcp.Core.Heuristics.LocalSearches.Afvs.Wrappers;
+
+namespace Anpcp.Core.UnitTests.Heuristics.LocalSearches.Afvs;
+
+public class LargestTwoGainsTests
+{
+    public static object[][] Data { get; } = [
+        [
+            new Dictionary<int, int>
+            {
+                { 0, 1 },
+                { 1, 0 },
+            },
+            new GainsFacility(0, 1),
+            new GainsFacility(1, 0)
+        ],
+        [
+            new Dictionary<int, int>
+            {
+                { 0, 0 },
+                { 1, 9 },
+                { 2, 2 },
+                { 3, 5 },
+            },
+            new GainsFacility(1, 9),
+            new GainsFacility(3, 5)
+        ]
+    ];
+
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void Constructor_SetsFirstAndSecond(
+        Dictionary<int, int> stubGainsNeighbors,
+        GainsFacility expectedFirst,
+        GainsFacility expectedSecond)
+    {
+        var stubLargestTwoGains = new LargestTwoGains(stubGainsNeighbors);
+
+        var actualFirst = stubLargestTwoGains.First;
+        var actualSecond = stubLargestTwoGains.Second;
+
+        Assert.Equal(expectedFirst, actualFirst);
+        Assert.Equal(expectedSecond, actualSecond);
+    }
+}
---
+++ src/Anpcp.Core/Heuristics/LocalSearches/Afvs/Wrappers/LargestTwoGains.cs
@@ -0,0 +1,40 @@
+﻿using Anpcp.Core.Heuristics.LocalSearches.Afvs.Models;
+
+namespace Anpcp.Core.Heuristics.LocalSearches.Afvs.Wrappers;
+
+/// <summary>
+/// Wraps the largest 2 gains from r(.)
+/// </summary>
+public class LargestTwoGains
+{
+    public GainsFacility First { get; private set; } = new(-1, -1);
+    public GainsFacility Second { get; private set; } = new(-1, -1);
+
+    public LargestTwoGains(Dictionary<int, int> gainsNeighbors)
+    {
+        // O(p)
+        foreach (var (facilityId, objectiveFunctionValue) in gainsNeighbors)
+        {
+            TryUpdate(facilityId, objectiveFunctionValue);
+        }
+    }
+
+    private bool TryUpdate(int facilityId, int objectiveFunctionValue)
+    {
+        var didUpdate = false;
+
+        if (objectiveFunctionValue > First.ObjectiveFunctionValue)
+        {
+            Second = First;
+            First = new(facilityId, objectiveFunctionValue);
+            didUpdate = true;
+        }
+        else if (objectiveFunctionValue > Second.ObjectiveFunctionValue)
+        {
+            Second = new(facilityId, objectiveFunctionValue);
+            didUpdate = true;
+        }
+
+        return didUpdate;
+    }
+}
