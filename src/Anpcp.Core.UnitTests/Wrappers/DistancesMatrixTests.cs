fb03828afc6f25f0aa5b9132ab4b7e21812e8808
netotz <neto.otz@hotmail.com>
2024-11-08 12:45:54 -0600

    feat(wrappers): add method to get sorted facilities by distance

--- src/Anpcp.Core.UnitTests/Wrappers/DistancesMatrixTests.cs
+++ src/Anpcp.Core.UnitTests/Wrappers/DistancesMatrixTests.cs
@@ -5,24 +5,55 @@ namespace Anpcp.Core.UnitTests.Wrappers;
 
 public class DistancesMatrixTests
 {
+    public static Vertex[] OneOriginVertices { get; } = [
+        new(0, 0, 0)
+    ];
+    public static Vertex[] TwoVertices1 { get; } = [
+        ..OneOriginVertices,
+        new(1, 0, 1)
+    ];
+    public static Vertex[] TwoVertices2 { get; } = [
+        new(0, 3, 4),
+        new(1, 5, 12)
+    ];
+    public static Vertex[] ThreeVertices1 { get; } = [
+        ..TwoVertices1,
+        new(2, 0, 2)
+    ];
+    public static Vertex[] ThreeVertices2 { get; } = [
+        new(0, 8, 15),
+        new(1, 5, 12),
+        new(2, 7, 24)
+    ];
+
     public static object[][] Data { get; } = [
         // 1 x 1, same set
         [
-            new Vertex[] { new(0, 0, 0) },
-            new Vertex[] { new(0, 0, 0) },
+            OneOriginVertices,
+            OneOriginVertices,
             new int[,] { { 0 } },
             (0, 0)
         ],
         // 2 x 2, different sets
         [
-            new Vertex[] { new(0, 0, 0), new(1, 0, 1) },
-            new Vertex[] { new(0, 3, 4), new(1, 5, 12) },
+            TwoVertices1,
+            TwoVertices2,
             new int[,] {
                 { 5, 13 },
                 { 4, 12 },
             },
             (0, 1)
         ],
+        [
+            ThreeVertices1,
+            ThreeVertices2,
+            new int[,] {
+                { 17, 13, 25 },
+                { 16, 12, 24 },
+                { 15, 11, 23 },
+            },
+            (0, 2)
+        ]
     ];
 
     [Theory]
@@ -48,5 +79,41 @@ public class DistancesMatrixTests
         }
 
         Assert.True(areEquals.All(a => a));
+    }
+
+    public static object[][] SortedData { get; } = [
+        [
+            new DistancesMatrix(TwoVertices1, TwoVertices2),
+            new int[][]
+            {
+                [0, 1],
+                [0, 1],
+            }
+        ],
+        [
+            new DistancesMatrix(ThreeVertices1, ThreeVertices2),
+            new int[][]
+            {
+                [1, 0, 2],
+                [1, 0, 2],
+                [1, 0, 2],
+            }
+        ]
+    ];
+
+    [Theory]
+    [MemberData(nameof(SortedData))]
+    public void GetNextNearestFacility_ReturnsSortedRow(
+        DistancesMatrix distancesMatrix,
+        int[][] expectedFacilityIds)
+    {
+        for (var i = 0; i < expectedFacilityIds.Length; i++)
+        {
+            var actualFacilityIds = distancesMatrix
+                .GetNextNearestFacility(i)
+                .ToArray();
+
+            Assert.Equal(expectedFacilityIds[i], actualFacilityIds);
+        }
     }
 }
--- src/Anpcp.Core/Wrappers/DistancesMatrix.cs
+++ src/Anpcp.Core/Wrappers/DistancesMatrix.cs
@@ -5,11 +5,16 @@ namespace Anpcp.Core.Wrappers;
 public class DistancesMatrix
 {
     private readonly int[,] _distances;
+    /// <summary>
+    /// Sorted distances where each cell is a tuple <c>(distance, facilityId)</c>.
+    /// </summary>
+    private readonly (int, int)[][] _sortedDistances;
 
     public int this[int fromIndex, int toIndex]
         => _distances[fromIndex, toIndex];
 
     public bool IsInitialized => _distances?.Length > 0;
+    public (int, int) Dimensions => (_distances.GetLength(0), _distances.GetLength(1));
 
     /// <summary>
     /// Indices pair of vertices whose distance is the maximum in the matrix.
@@ -19,19 +24,29 @@ public class DistancesMatrix
     public DistancesMatrix()
     {
         _distances = new int[0, 0];
+        _sortedDistances = [];
         MaxPair = (-1, -1);
     }
 
     public DistancesMatrix(Vertex[] vertices1, Vertex[] vertices2)
     {
-        _distances = new int[vertices1.Length, vertices2.Length];
+        var n = vertices1.Length;
+        var m = vertices2.Length;
+
+        _distances = new int[n, m];
+        _sortedDistances = new (int, int)[n][];
 
         // current maximum distance in the matrix
         var currentMax = int.MinValue;
 
-        for (var i = 0; i < vertices1.Length; i++)
+        //// O(nm log m)
+        // O(n)
+        for (var i = 0; i < n; i++)
         {
-            for (var j = 0; j < vertices2.Length; j++)
+            var unsortedRow = new List<int>(m);
+
+            // O(m)
+            for (var j = 0; j < m; j++)
             {
                 var distance = (int)vertices1[i].DistanceTo(vertices2[j]);
                 _distances[i, j] = distance;
@@ -41,7 +56,25 @@ public class DistancesMatrix
                     currentMax = distance;
                     MaxPair = (i, j);
                 }
+
+                unsortedRow.Add(distance);
             }
+
+            // O(m log m)
+            var sortedRow = unsortedRow
+                .Select((distance, index) => (distance, index))
+                .OrderBy(s => s.distance)
+                .ToArray();
+
+            _sortedDistances[i] = sortedRow;
+        }
+    }
+
+    public IEnumerable<int> GetNextNearestFacility(int fromUser)
+    {
+        foreach (var (_, facilityId) in _sortedDistances[fromUser])
+        {
+            yield return facilityId;
         }
     }
 }
