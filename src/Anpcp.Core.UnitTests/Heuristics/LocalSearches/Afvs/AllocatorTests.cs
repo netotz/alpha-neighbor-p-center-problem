5b299925693ed3d73b77f5c48ee3e71cec7b3000
netotz <neto.otz@hotmail.com>
2024-11-10 00:24:16 -0600

    feat(afvs): add `Allocator` helper class

---
+++ src/Anpcp.Core.UnitTests/Heuristics/LocalSearches/Afvs/AllocatorTests.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Immutable;
+
+using Anpcp.Core.Heuristics.LocalSearches.Afvs;
+
+namespace Anpcp.Core.UnitTests.Heuristics.LocalSearches.Afvs;
+
+public class AllocatorTests
+{
+    public static object[][] Data { get; } = [
+        [
+            2, 2, 2,
+            Enumerable.Range(0, 2).ToImmutableHashSet(),
+            (int u) => Enumerable.Range(0, 2),
+            new HashSet<int>(),
+            // no centers, all allocations should = 0
+            new int[,] {
+                { 0, 0 },
+                { 0, 0 },
+            }
+        ],
+        [
+            2, 2, 3,
+            Enumerable.Range(0, 2).ToImmutableHashSet(),
+            (int u) => Enumerable.Range(0, 3),
+            // only facilities 0, 1 are centers
+            Enumerable.Range(0, 2).ToHashSet(),
+            new int[,] {
+                { 1, 2, 0 },
+                { 1, 2, 0 },
+            }
+        ],
+        [
+            2, 3, 4,
+            Enumerable.Range(0, 3).ToImmutableHashSet(),
+            // arbitrary "nearest" facilities
+            (int u) => u switch {
+                0 => new []{ 0, 1, 2, 3 },
+                1 => [3, 2, 1, 0],
+                2 => [0, 2, 1, 3],
+                _ => []
+            },
+            Enumerable.Range(0, 3).ToHashSet(),
+            new int[,] {
+                { 1, 2, 3, 0 },
+                { 3, 2, 1, 0 },
+                { 1, 3, 2, 0 }
+            }
+        ],
+    ];
+
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void AllocateAll_CorrectlyFillsAllocationsMatrix(
+        int alpha, int n, int m,
+        ImmutableHashSet<int> fakeUserIds,
+        Func<int, IEnumerable<int>> fakeNearestFacilitiesGetter,
+        HashSet<int> fakeCenters,
+        int[,] expectedAllocationsMatrix)
+    {
+        var mockAllocator = new Allocator(alpha, n, m);
+
+        mockAllocator.AllocateAll(fakeUserIds, fakeNearestFacilitiesGetter, fakeCenters);
+
+        for (var i = 0; i < n; i++)
+        {
+            for (var j = 0; j < m; j++)
+            {
+                Assert.True(expectedAllocationsMatrix[i, j] == mockAllocator[i, j]);
+            }
+        }
+    }
+}
---
+++ src/Anpcp.Core/Heuristics/LocalSearches/Afvs/Allocator.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Immutable;
+
+namespace Anpcp.Core.Heuristics.LocalSearches.Afvs;
+
+public class Allocator(int alpha, int n, int m)
+{
+    private readonly int[,] _allocations = new int[n, m];
+
+    /// <summary>
+    /// Gets a value in the allocations matrix.
+    /// </summary>
+    public int this[int userId, int facilityId] => _allocations[userId, facilityId];
+
+    /// <summary>
+    /// Allocates all users to their alpha-neighbors.
+    /// </summary>
+    /// <param name="nearestFacilitiesGetter">
+    /// A delegate that enumerates in order the nearest facilities of a user.
+    /// </param>
+    /// <remarks>Time O(nm)</remarks>
+    public void AllocateAll(
+        ImmutableHashSet<int> userIds,
+        Func<int, IEnumerable<int>> nearestFacilitiesGetter,
+        HashSet<int> centers)
+    {
+        //// O(nm)
+        // O(n)
+        foreach (var userId in userIds)
+        {
+            var nearestFacilities = nearestFacilitiesGetter(userId);
+            // O(m)
+            ReallocateUser(userId, nearestFacilities, centers);
+        }
+    }
+
+    /// <remarks>Time O(m)</remarks>
+    private void ReallocateUser(
+        int userId,
+        IEnumerable<int> nearestFacilities,
+        HashSet<int> centers)
+    {
+        // O(m)
+        DeallocateUser(userId);
+
+        var proximity = 0;
+        // O(m)
+        foreach (var facilityId in nearestFacilities)
+        {
+            // no need to allocate facilities farther than (alpha+1)-th
+            if (proximity > alpha + 1)
+            {
+                break;
+            }
+
+            // ignore facilities outside solution
+            if (!centers.Contains(facilityId))
+            {
+                continue;
+            }
+
+            proximity++;
+            _allocations[userId, facilityId] = proximity;
+        }
+    }
+
+    /// <summary>
+    /// Deallocates a user from all facilities.
+    /// </summary>
+    /// <remarks>Time O(m)</remarks>
+    private void DeallocateUser(int userId)
+    {
+        // O(m)
+        for (var j = 0; j < m; j++)
+        {
+            _allocations[userId, j] = 0;
+        }
+    }
+}
