3f5d0708a9f25bffc3bcf6928921e4a7e342a6bb
netotz <neto.otz@hotmail.com>
2024-11-18 23:21:49 -0600

    feat(grasp): add RCL class
    part of #19

---
+++ src/Anpcp.Core.UnitTests/Heuristics/Constructives/RestrictedCandidateListTests.cs
@@ -0,0 +1,38 @@
+﻿using Anpcp.Core.Heuristics.Constructives.Rgd;
+
+namespace Anpcp.Core.UnitTests.Heuristics.Constructives;
+
+public class RestrictedCandidateListTests
+{
+    public const int BETA_0 = 0;
+    public static int TestSeed => 0;
+
+    public static object[][] Data { get; } = [
+        [
+            new [] {
+                (0, 20),
+                (1, 10),
+                (2, 30),
+            },
+            2
+        ]
+    ];
+
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void GetFacilityToInsert_WhenBetaIs0_ReturnsFacilityWithMaxDistance(
+        (int, int)[] candidates,
+        int expectedFacilityToInsert)
+    {
+        var stubRestrictedCandidateList = new RestrictedCandidateList(BETA_0, TestSeed);
+
+        foreach (var (facilityId, distance) in candidates)
+        {
+            stubRestrictedCandidateList.Add(facilityId, distance);
+        }
+
+        var actualFacilityToInsert = stubRestrictedCandidateList.GetFacilityToInsert();
+
+        Assert.Equal(expectedFacilityToInsert, actualFacilityToInsert);
+    }
+}
---
+++ src/Anpcp.Core/Heuristics/Constructives/Rgd/RestrictedCandidateList.cs
@@ -0,0 +1,49 @@
+﻿namespace Anpcp.Core.Heuristics.Constructives.Rgd;
+
+/// <summary>
+/// Restricted Candidate List (RCL) component for a constructive heuristic within GRASP.
+/// </summary>
+/// <param name="beta">GRASP parameter.</param>
+/// <param name="seed">Seed for random operations.</param>
+public class RestrictedCandidateList(float beta, int seed)
+{
+    private readonly List<(int FacilityId, int Distance)> _candidates = [];
+
+    private int MaxCost { get; set; } = int.MinValue;
+    private int MinCost { get; set; } = int.MaxValue;
+    private float? Threshold { get; set; }
+
+    /// <summary>
+    /// Adds a facility to the RCL and updates the maximum and minimum costs,
+    /// which will be used to calculate the threshold.
+    /// </summary>
+    public void Add(int facilityId, int distance)
+    {
+        _candidates.Add((facilityId, distance));
+
+        MaxCost = Math.Max(MaxCost, distance);
+        MinCost = Math.Min(MinCost, distance);
+    }
+
+    /// <summary>
+    /// Gets at random a facility to insert, following the threshold formula.
+    /// </summary>
+    /// <returns>ID of the facility to insert.</returns>
+    /// <remarks>Time O(m)</remarks>
+    public int GetFacilityToInsert()
+    {
+        Threshold = MaxCost - (beta * (MaxCost - MinCost));
+
+        var random = new Random(seed);
+
+        // O(m)
+        var thresholdedCandidateIds = _candidates
+            .Where(c => c.Distance >= Threshold)
+            .Select(c => c.FacilityId)
+            .ToArray();
+
+        var randomIndex = random.Next(thresholdedCandidateIds.Length);
+
+        return thresholdedCandidateIds[randomIndex];
+    }
+}
