28dd036c36fde58f1d5710dcc6ce7f2e9848c8f8
netotz <neto.otz@hotmail.com>
2024-12-15 23:51:14 -0600

    feat(grasp): add GRASP core classes

---
+++ src/Anpcp.Core/Heuristics/Metaheuristics/GraspRgdAfvs.cs
@@ -0,0 +1,18 @@
+﻿using Anpcp.Core.Heuristics.Constructives.Rgd;
+using Anpcp.Core.Heuristics.LocalSearches.Afvs;
+using Anpcp.Core.Instances;
+
+namespace Anpcp.Core.Heuristics.Metaheuristics;
+
+public class GraspRgdAfvs(
+    int maxIterations,
+    TimeSpan timeLimit,
+    RandomizedGreedyDispersion<InstanceTwoSets> constructive,
+    AlphaFastVertexSubstitution localSearch)
+    : BaseGrasp<
+        InstanceTwoSets,
+        RandomizedGreedyDispersion<InstanceTwoSets>,
+        AlphaFastVertexSubstitution>(maxIterations, timeLimit, constructive, localSearch)
+{
+
+}
